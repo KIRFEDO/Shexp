c69e08e28e08d7d36d4b97483862d15958465f19
Kiryl Fedaryshkin <kfedorishkin@mail.ru>
2022-05-01 19:54:33 +0200

    test commit

---
+++ app/src/main/java/com/example/myapplication/MainPageActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+
+
+
+}
