5b24692fe2700e5f76c4a167ea479b7f298702a4
Kiryl Fedaryshkin <kfedorishkin@mail.ru>
2022-05-01 22:02:28 +0200

    Main+Login Page

--- app/src/main/java/com/example/myapplication/ActivityLogin.java
+++ app/src/main/java/com/example/myapplication/ActivityLogin.java
@@ -3,16 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainPageActivity extends AppCompatActivity {
+public class ActivityLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_page);
+        setContentView(R.layout.activity_login);
     }
-
-
-
 }
---
+++ app/src/main/java/com/example/myapplication/ActivityMain.java
@@ -0,0 +1,49 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ActivityMain extends AppCompatActivity {
+
+    private LinearLayout LL;
+    private TextView TV;
+    private Button AM_Login;
+    private Button AM_Register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LL = findViewById(R.id.AM_LL);
+        TV = findViewById(R.id.AM_TV);
+        AM_Login = findViewById(R.id.AM_Login);
+        AM_Register = findViewById(R.id.AM_Register);
+
+        TV.startAnimation(AnimationUtils.loadAnimation(
+                getApplicationContext(),
+                R.anim.mp_move_logo_up
+        ));
+        LL.startAnimation(AnimationUtils.loadAnimation(
+                getApplicationContext(),
+                R.anim.mp_btn_show
+        ));
+        AM_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ActivityLogin.class);
+                startActivity(intent);
+            }
+        });
+    };
+
+}
